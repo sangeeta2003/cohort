0c610fc808904899fd58c4618de2a2b3d700e57c
sangeeta2003 <mishrasangeeta944@gmail.com>
2023-12-12 18:40:29 +0530

    callback function

--- callback.js
+++ callback.js
@@ -4,10 +4,20 @@ return n*n;
 function cube(n){
     return n*n*n;
 }
-function sumOfSquare(a,b){
-    let square1 = square(a);
-    let square2 = square(b);
+// generic
+function sumOfSquare(a,b,fn){
+    let square1 = fn(a);
+    let square2 = fn(b);
     return  square1 + square2;
 }
-let result = sumOfSquare(2,3);
+function sumofQube(a,b,fncallback){
+    let cube1 = fncallback(a);
+    let cube2 = fncallback(b);
+    return cube1 + cube2;
+}
+// passing the function as argument
+let result1 = sumofQube(1,2,cube)
+console.log(result1);
+// functional arguments
+let result = sumOfSquare(2,3,square);
 console.log(result);
