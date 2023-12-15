99286ad35639f54b794e95ee5c48d3e0310d57d0
*pknowledge* <mishrasangeeta944@gmail.com>
2023-12-15 21:59:18 +0530

    filter.js

---
+++ fronted/filter.js
@@ -0,0 +1,16 @@
+const arr = [1,2,3,4,5];
+// ans
+const ans = arr.filter((n)=>{
+    if(n%2 == 0){
+        return true
+        }
+        else {
+            return false;
+        }
+});
+
+
+
+
+
+console.log(ans);
