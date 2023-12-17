3749c1a3b87d2789725efed6612b25baca93a9f9
*pknowledge* <mishrasangeeta944@gmail.com>
2023-12-17 18:27:08 +0530

    express.js

---
+++ backend/express.js
@@ -0,0 +1,17 @@
+// creating an http server
+const express = require('express');
+const app = express();
+
+function sum(n){
+    let ans = 0;
+for(let i= 0;i<=n;i++){
+ans = ans+i;
+}
+}
+app.get('/',function(req,res){
+    const n = req.query.n;
+    const ans = sum(n);
+    res.send("hy there" + ans)
+})
+app.listen(3000);
+console.log("run the server")
