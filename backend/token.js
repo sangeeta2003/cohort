5743da498b0b0aa5b2113dd9535aebe23b006092
*pknowledge* <mishrasangeeta944@gmail.com>
2023-12-21 18:34:18 +0530

    jwt

---
+++ backend/token.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const jwtPassword = "12345";
+const app = express()
+const ALL_VALUES = [
+    {
+        username:"sangeeta123@gmail.com",
+        password :'123',
+        name:"sangeeta mishra"
+    },
+    {
+        username:"Ram123@gmail.com",
+        password :'12331',
+        name:"sangeeta mishra"
+    },
+    {
+        username:"priya123@gmail.com",
+    password :'1234',
+    name:"sangeeta mishra"
+},
+
+];
+function userExit(username,password){
+    // write the logic to return true or false if this user exit 
+    // in all_users array
+}
+app.post('/signin',function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+
+    if(!userExit(username,password)){
+        return res.status(403).json({
+            msg:"user doesn't exit in our in memory db"
+        })
+    }
+    var token = jwt.sign({username :username},"shhh");
+    return res.json({
+        token,
+    })
+}
+);
+app.get('/users',function(req,res){
+    const token = req.headers.authorization;
+    try{
+        const decoded = jwt.verify(token.jwtPassword);
+        const username = decoded.username;
+        // return a list of users other than this users
+
+    }
+    catch(err){
+return res.status(403).json({
+    msg:"invalid token",
+})
+    }
+})
+app.listen(3000)
