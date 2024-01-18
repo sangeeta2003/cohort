a7293dfd24e1a009f9646e2cdad07108373d584c
*pknowledge* <mishrasangeeta944@gmail.com>
2024-01-18 20:39:20 +0530

    jwt

---
+++ backend/jwt.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken")
+
+
+// decode,verify,generate
+const value = {
+    name:"sangeeta",
+    accountnumber : 123123
+}
+// sign is not generate
+const token = jwt.sign(value,"secret")
+console.log(token);
+
+// this token be generated using this secret and verified using this secret
+
+// verify
+const verifyvalue = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic2FuZ2VldGEiLCJhY2NvdW50bnVtYmVyIjoxMjMxMjMsImlhdCI6MTcwNTU5MDA5Nn0.4IfmBy4s6_C1eEDcrAGfSdmnxm6GbYlxLxcPELbgRww","secret")
+console.log(verifyvalue);
