a860d146b072ed218cdd8203c9d4d817aeaf02b2
*pknowledge* <mishrasangeeta944@gmail.com>
2024-01-19 20:53:29 +0530

    jwt

--- backend/jwt.js
+++ backend/jwt.js
@@ -11,7 +11,7 @@ const token = jwt.sign(value,"secret")
 console.log(token);
 
 // this token be generated using this secret and verified using this secret
-
+// jwt 
 // verify
 const verifyvalue = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic2FuZ2VldGEiLCJhY2NvdW50bnVtYmVyIjoxMjMxMjMsImlhdCI6MTcwNTU5MDA5Nn0.4IfmBy4s6_C1eEDcrAGfSdmnxm6GbYlxLxcPELbgRww","secret")
 console.log(verifyvalue);
