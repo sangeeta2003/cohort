9aa2e4a1b12a8341a37de023422b7db55c700a95
*pknowledge* <mishrasangeeta944@gmail.com>
2024-02-19 21:53:29 +0530

    react hooks

---
+++ fronted/React/react-hook/src/App.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useState } from 'react';
+let counter = 4;
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'go to gym',
+      description: 'go to gym today',
+    },
+    {
+      id: 2,
+      title: 'eat food',
+      description: 'eat food',
+    },
+    {
+      id: 3,
+      title: 'go to class',
+      description: 'go to class',
+    },
+  ]);
+
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        id: counter++,
+        title: `New Todo ${Math.random()}`,
+        description: 'New todo description',
+      },
+    ]);
+  }
+
+  return (
+    <div>
+      <button onClick={addTodo}>Add a todo</button>
+      {todos.map((todo) => (
+        <Todo key={todo.id} {...todo} />
+      ))}
+    </div>
+  );
+}
+
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
+}
+
+export default App;
