939a8676076193013b54a44885a27d54f6b2e21a
*pknowledge* <mishrasangeeta944@gmail.com>
2024-02-21 20:57:06 +0530

    prop drilling

--- fronted/React/Routes/route/src/App.jsx
+++ fronted/React/Routes/route/src/App.jsx
@@ -59,36 +59,48 @@
 
 
 // prop drilling 
-import { useState } from "react"
+import { useState, useContext } from "react";
+import { CountContext } from './components/Context';
 
 function App() {
-  const [count,setCount]= useState(0)
+  const [count, setCount] = useState(0);
+
+  return (
+    <CountContext.Provider value={{ count, setCount }}>
+      <Count />
+    </CountContext.Provider>
+  );
+}
+
+function Count() {
   return (
     <div>
-<Count count={count}/>
-<Buttons count={count} setCount={setCount}/>
+      <CountRenderer />
+      <Buttons />
     </div>
-  )
-}   
-
-function Count({count}){
-  return <div>
-{count}
-  </div>
+  );
 }
-function Buttons({count,setCount}){
-  return <div>
-    <button onClick={()=>{
-setCount(count+1)
-    }}>
-      increase
-    </button>
-    <button onClick={()=>{
-      setCount(count-1)
-    }}>
-      decrease
-    </button>
-  </div>
+// context api
+function CountRenderer() {
+  const { count } = useContext(CountContext);
+
+  return <div>{count}</div>;
 }
 
-export default App
+function Buttons() {
+  const { setCount } = useContext(CountContext);
+
+  return (
+    <div>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increase
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
+ 
